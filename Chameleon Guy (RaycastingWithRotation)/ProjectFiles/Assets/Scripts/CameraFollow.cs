af16faaa1a5e0b0e1c63c02f7ad4309e939255c4
uabhanu <uabhanu@gmail.com>
2018-01-28 11:22:38 +0000

    Chameleon Guy (RaycastingWithRotation) Up Next

---
+++ Chameleon Guy (RaycastingWithRotation)/ProjectFiles/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+	private Rigidbody cameraBody , playerBody;
+
+	void Start() 
+	{
+		cameraBody = GetComponent<Rigidbody>();
+		playerBody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() 
+	{
+		cameraBody.velocity = new Vector3(playerBody.velocity.x , 0f , 0f);
+	}
+}
---
+++ Chameleon Guy (RaycastingWithRotation)/ProjectFiles/Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+	public GameObject helpButton , helpObj;
+
+	void Start() 
+	{
+	
+	}
+
+	public void Continue()
+	{
+		helpButton.SetActive(true);
+		helpObj.SetActive(false);
+	}
+
+	public void Help()
+	{
+		helpButton.SetActive(false);
+		helpObj.SetActive(true);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
