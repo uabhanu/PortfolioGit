61fedf804e4745eb819767be67fa405b17692a43
uabhanu <uabhanu@gmail.com>
2018-01-29 19:12:40 +0000

    Colouring & JSon Added

---
+++ Colouring & JSon/ProjectFiles/Assets/Scripts/BhanuQuad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BhanuQuad : MonoBehaviour 
+{
+	MeshRenderer quadRenderer;
+
+	void Start() 
+	{
+		quadRenderer = GetComponent<MeshRenderer>();
+		quadRenderer.materials[1].color = Color.blue;
+	}
+
+	void Update() 
+	{
+	
+	}
+}
