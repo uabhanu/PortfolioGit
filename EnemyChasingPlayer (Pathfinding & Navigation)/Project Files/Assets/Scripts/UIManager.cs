aa897deff1c8bf619ee56d6fc26fbe7d224b237e
uabhanu <uabhanu@gmail.com>
2018-01-29 19:54:28 +0000

    EnemyChasingPlayer (Pathfinding & Navigation) Added

---
+++ EnemyChasingPlayer (Pathfinding & Navigation)/Project Files/Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+	public GameObject helpButton , helpObj , quitButton;
+
+	void Start() 
+	{
+	
+	}
+
+	public void Continue()
+	{
+		//Always create UI freshly to avoid complications
+		helpButton.SetActive(true);
+		helpObj.SetActive(false);
+		quitButton.SetActive(true);
+	}
+
+	public void Help()
+	{
+		//Always create UI freshly to avoid complications
+		helpButton.SetActive(false);
+		helpObj.SetActive(true);
+		quitButton.SetActive(false);
+	}
+
+	public void Quit()
+	{
+		//Always create UI freshly to avoid complications
+		Application.Quit();
+	}
+}
