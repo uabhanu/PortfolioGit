cb5bc52c6198df024da8f48ece35e068ec1082c6
uabhanu <uabhanu@gmail.com>
2018-03-10 20:18:51 +0000

    BhanuPathFinding Enemy Chasing Player WIP Obstruction Logic Up Next

---
+++ BhanuPathFinding/Assets/Scripts/Bhanu/BhanuPath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BhanuPath : MonoBehaviour 
+{
+    [SerializeField] float m_moveSpeed , m_stoppingDistance;
+    [SerializeField] GameObject m_playerToChase;
+
+	void Start() 
+    {
+		
+	}
+	
+	void Update() 
+    {
+        if(Time.timeScale == 0)
+        {
+            return;
+        }
+        
+        //TODO Chase Player Logic here, refer Bookmark
+	}
+}
