944a587e50b655b2bd72f80a3896862fa799b5cf
uabhanu <uabhanu@gmail.com>
2018-03-11 11:20:06 +0000

    Layer Collision Matrix Updated & Cube Layer changed to Enemy Enemy Follow Logic Complete Obstruction & Evasion Up Next

--- BhanuPathFinding/Assets/Scripts/Bhanu/BhanuPath.cs
+++ BhanuPathFinding/Assets/Scripts/Bhanu/BhanuPath.cs
@@ -2,8 +2,8 @@
 
 public class BhanuPath : MonoBehaviour 
 {
-    [SerializeField] float m_moveSpeed , m_stoppingDistance;
-    [SerializeField] GameObject m_playerToChase;
+    [SerializeField] float m_moveSpeed , m_offset;
+    [SerializeField] Transform m_playerToLookAt;
 
 	void Start() 
     {
@@ -16,7 +16,12 @@ public class BhanuPath : MonoBehaviour
         {
             return;
         }
-        
-        //TODO Chase Player Logic here, refer Bookmark
-	}
+
+        transform.LookAt(m_playerToLookAt.position);
+
+        if((transform.position - m_playerToLookAt.position).magnitude > m_offset)
+        {
+            transform.Translate(0f , 0f , m_moveSpeed * Time.deltaTime);
+        }
+    }
 }
