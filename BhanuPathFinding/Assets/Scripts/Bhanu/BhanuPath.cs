fd01f82155e74fed538e1a97c3a65a6360f7bed2
uabhanu <uabhanu@gmail.com>
2018-03-11 19:42:10 +0000

    Avoid Obstacle WIP

--- BhanuPathFinding/Assets/Scripts/Bhanu/BhanuPath.cs
+++ BhanuPathFinding/Assets/Scripts/Bhanu/BhanuPath.cs
@@ -2,6 +2,7 @@
 
 public class BhanuPath : MonoBehaviour 
 {
+    [SerializeField] bool _isFollowingPlayer , _obstacleDetected;
     Ray _ray;
     RaycastHit _hit;
 
@@ -20,11 +21,12 @@ public class BhanuPath : MonoBehaviour
             return;
         }
 
-        AvoidObstacles();
+        DetectObstacle();
+        Evade();
         FollowPlayer();
     }
 
-    void AvoidObstacles()
+    void DetectObstacle()
     {
         Debug.DrawRay(transform.position , transform.TransformDirection(Vector3.forward) * _lineOfSight , Color.red);
         _ray = new Ray(transform.position , transform.TransformDirection(Vector3.forward) * _lineOfSight);
@@ -33,8 +35,23 @@ public class BhanuPath : MonoBehaviour
         {
             if(_hit.collider.gameObject.layer == 11)
             {
-                //TODO Evade Logic here, Look into Slerp
+                _isFollowingPlayer = false;
+                _obstacleDetected = true;
             }
+        }
+        else
+        {
+            _isFollowingPlayer = true;
+            _obstacleDetected = false;
+        }
+    }
+
+    void Evade()
+    {
+        if(_obstacleDetected)
+        {
+            //TODO For some reason, this is working on it's own but once movement logic exists, this doesn't work, figure it out
+            transform.Rotate(Vector3.up * 15f * Time.deltaTime);
         }
     }
 
@@ -42,8 +59,9 @@ public class BhanuPath : MonoBehaviour
     {
         transform.LookAt(_playerToLookAt.position);
 
-        if((transform.position - _playerToLookAt.position).magnitude > _offset)
+        if((transform.position - _playerToLookAt.position).magnitude > _offset && _isFollowingPlayer)
         {
+            //TODO For some reason movement in z axis is not stopping even after this is stopped, figure it out
             transform.Translate(0f , 0f , _moveSpeed * Time.deltaTime);
         }
     }
